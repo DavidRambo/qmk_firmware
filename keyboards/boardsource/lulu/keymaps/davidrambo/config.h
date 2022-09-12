6a8c05da25a68e5f02770ef3b695c9eb154e384a
David Rambo <davrambo@gmail.com>
2022-09-12 10:30:40 -0700

    disable rgb when usb suspend

--- keyboards/boardsource/lulu/keymaps/davidrambo/config.h
+++ keyboards/boardsource/lulu/keymaps/davidrambo/config.h
@@ -3,3 +3,7 @@
 #define MASTER_RIGHT
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 #define CAPS_WORD_IDLE_TIMEOUT 3000  // 3 seconds.
+
+#ifdef RGB_MATRIX_ENABLE
+    #define rgb_disable_when_usb_suspended  // turn off effects when suspended
+#endif
