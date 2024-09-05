15affc879416beb545b2afcf8c6cecc4aa545358
David Rambo <davidrambo@mailfence.com>
2024-09-05 10:04:07 -0700

    Add note re flashing the right side.

--- keyboards/crkbd/keymaps/davidrambo/rules.mk
+++ keyboards/crkbd/keymaps/davidrambo/rules.mk
@@ -1,1 +1,7 @@
+# My Corne's left hand has an Elite-C, whereas the right hand has a micro-usb.
+# This bootloader does not pick up the latter when trying to flash the right side.
+# (This was required when I recently reflashed in order to get the right hand LEDs to work.)
+# To flash the right side, plug in a micro-usb and run:
+# $ make crkbd:davidrambo:avrdude-split-right
+
 BOOTLOADER = atmel-dfu
