90c1f433d369bf16053f0d420a6d409e2027283d
David Rambo <davidrambo@mailfence.com>
2026-06-16 12:40:41 -0700

    rm copy, paste from nav layer

--- keyboards/boardsource/lulu/keymaps/davidrambo/keymap.c
+++ keyboards/boardsource/lulu/keymaps/davidrambo/keymap.c
@@ -211,7 +211,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NAVIGATION] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
   _______, _______, KC_BTN2, KC_MS_U, KC_BTN1, _______,                      C_TAB  , CLEFT  ,  KC_UP , CRGHT  , KC_DEL , _______,
-  _______, KC_COPY, KC_MS_L, KC_MS_D, KC_MS_R, KC_PSTE,                      CTLPGUP, KC_LEFT, KC_DOWN, KC_RGHT, CTLPGDN, _______,
+  _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,                      CTLPGUP, KC_LEFT, KC_DOWN, KC_RGHT, CTLPGDN, _______,
   _______, KC_HOME, KC_END , KC_WH_U, KC_WH_D, KC_MPLY,  _______,  _______,  A_TAB  , KC_PGDN, KC_PGUP, _______, G_GRV  , _______,
                             _______, _______, _______,  _______,    _______,  _______, _______, _______
 ),
