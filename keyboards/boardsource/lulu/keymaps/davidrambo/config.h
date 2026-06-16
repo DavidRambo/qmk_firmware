1d671f63429c7d6a6bed81c00ee4353e32195f0e
David Rambo <davidrambo@mailfence.com>
2026-06-16 12:35:16 -0700

    increase caps word timeout

--- keyboards/boardsource/lulu/keymaps/davidrambo/config.h
+++ keyboards/boardsource/lulu/keymaps/davidrambo/config.h
@@ -19,7 +19,7 @@
 
 #define MASTER_RIGHT
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
-#define CAPS_WORD_IDLE_TIMEOUT 3000  // 3 seconds.
+#define CAPS_WORD_IDLE_TIMEOUT 5000  // 5 seconds.
 
 #define TAPPING_TERM 190
 #define QUICK_TAP_TERM 50
