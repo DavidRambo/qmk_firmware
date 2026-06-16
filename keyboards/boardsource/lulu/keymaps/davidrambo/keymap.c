79d3871a0fdc50397e14747d1f31a8e760c90426
David Rambo <davidrambo@mailfence.com>
2026-06-16 12:36:59 -0700

    swap J and QUOT

--- keyboards/boardsource/lulu/keymaps/davidrambo/keymap.c
+++ keyboards/boardsource/lulu/keymaps/davidrambo/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_GRALMAK] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MPLY,
-  KC_TAB,   KC_B,   KC_L,    KC_D,    KC_W,    KC_Z,                     KC_J,  KC_F,    KC_O,    KC_U,    KC_QUOT, KC_BSPC,
+  KC_TAB,   KC_B,   KC_L,    KC_D,    KC_W,    KC_Z,                     KC_QUOT,KC_F,   KC_O,    KC_U,    KC_J,    KC_BSPC,
   ESCNAV,   HOME_N, HOME_R,  HOME_T,  HOME_S,  KC_G,                     KC_Y,  HOME_H,  HOME_A,  HOME_E,  HOME_I,  KC_SCLN,
   KC_LSFT,  KC_Q,   KC_X,    KC_M,    KC_C,    KC_V,   KC_MUTE,   KC_NO, KC_K,  KC_P,    KC_COMM, KC_DOT,  KC_SLSH, SftEnt,
                      MEH_T(KC_DEL), KC_LALT, ESCNAV, BSPCSYM,       SPCNUM, KC_UNDS, FKEY, KC_RALT
